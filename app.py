f37e6a5372237f457746e9f480d3864e66ea25b2
mujahidina <mujahidabdi5070@gmail.com>
2025-03-21 11:26:09 +0300

    added tekton pipeline

--- app.py
+++ app.py
@@ -94,5 +94,8 @@ class UserByID(Resource):
 api.add_resource(UserByID, "/users/<int:id>")
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
